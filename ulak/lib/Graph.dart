bf410711444de7b4183035baaa3eac1e4f250db6
umutyunusoglu <umut.yunusoglu@sabanciuniv.edu>
2024-01-01 23:37:09 +0300

    Basic Backbone of Data Structures

--- ulak/lib/Graph.dart
+++ ulak/lib/Graph.dart
@@ -1,10 +1,28 @@
 import 'package:geolocator/geolocator.dart';
 
+
+class Data{
+
+}
+
 class Node{
 
+  Data? data;
 
+  List<Node>? adjecentNodes;
+  
+  Node(){
+    data =Data();
+    adjecentNodes = [];
+  }
 
+  void scanForAdjecentNodes(){
+  
+  }
 
+  void updateData(){
+    
+  }
 
 }
 
