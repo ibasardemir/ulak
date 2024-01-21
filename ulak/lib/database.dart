74dd60940603149b82f8158a2440380000322ac5
umutyunusoglu <umut.yunusoglu@sabanciuniv.edu>
2024-01-21 19:20:51 +0300

    Fixed Some issues

--- ulak/lib/database.dart
+++ ulak/lib/database.dart
@@ -1,90 +1,64 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class Database {
-  Future<void> saveData(String key, dynamic data);
-  Future<dynamic> getData(String key);
-  Future<void> deleteData(String key);
+  Future<void> saveData(String tablename, String key, dynamic data);
+  Future<dynamic> getData(String tablename, String key);
+  Future<void> deleteData(String tablename, String key);
+  bool syncDatabases();
+
 }
 
-class LocalUsersDB extends Database{
+class LocalDB extends Database{
   @override
-  Future<void> deleteData(String key) {
+  Future<void> deleteData(String tablename, String key) {
     // TODO: implement deleteData
     throw UnimplementedError();
   }
 
   @override
-  Future getData(String key) {
+  Future getData(String tablename, String key) {
     // TODO: implement getData
     throw UnimplementedError();
   }
 
   @override
-  Future<void> saveData(String key, data) {
+  Future<void> saveData(String tablename, String key, data) {
     // TODO: implement saveData
     throw UnimplementedError();
   }
-}
-
-class LocalMessagesDB extends Database{
-  @override
-  Future<void> deleteData(String key) {
-    // TODO: implement deleteData
-    throw UnimplementedError();
-  }
 
   @override
-  Future getData(String key) {
-    // TODO: implement getData
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> saveData(String key, data) {
-    // TODO: implement saveData
+  bool syncDatabases() {
+    // TODO: implement syncDatabases
     throw UnimplementedError();
   }
 
 }
 
-class FirebaseUserDB extends Database{
+class FirebaseDB extends Database{
   @override
-  Future<void> deleteData(String key) {
+  Future<void> deleteData(String tablename, String key) {
     // TODO: implement deleteData
     throw UnimplementedError();
   }
 
   @override
-  Future getData(String key) {
+  Future getData(String tablename, String key) {
     // TODO: implement getData
     throw UnimplementedError();
   }
 
   @override
-  Future<void> saveData(String key, data) {
+  Future<void> saveData(String tablename, String key, data) {
     // TODO: implement saveData
     throw UnimplementedError();
   }
 
-}
-
-class FirebaseMessagesDB extends Database{
   @override
-  Future<void> deleteData(String key) {
-    // TODO: implement deleteData
+  bool syncDatabases() {
+    // TODO: implement syncDatabases
     throw UnimplementedError();
   }
 
-  @override
-  Future getData(String key) {
-    // TODO: implement getData
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> saveData(String key, data) {
-    // TODO: implement saveData
-    throw UnimplementedError();
-  }
 
 }
