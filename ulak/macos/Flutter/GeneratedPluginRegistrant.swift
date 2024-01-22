52f4dff3239586be05035abdb3651b1e379fb254
Basar <ibrahim.demir@sabanciuniv.edu>
2024-01-22 17:24:09 +0300

    update gitignore

--- ulak/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ulak/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import device_info_plus
 import firebase_core
 import shared_preferences_foundation
-import device_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
