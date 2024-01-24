6ce8bce593595c38c3f99b086db99a9e06773633
Basar <ibrahim.demir@sabanciuniv.edu>
2024-01-24 21:54:38 +0300

    add nearby service functions

--- ulak/lib/network/networkservice.dart
+++ ulak/lib/network/networkservice.dart
@@ -1,8 +1,9 @@
 import "dart:async";
-
+import "dart:math";
 import "package:flutter_nearby_connections/flutter_nearby_connections.dart";
 import "package:device_info_plus/device_info_plus.dart";
 import "package:ulak/database.dart";
+
 
 class Message {
   int time;String sender;String receiver; String content;
@@ -11,10 +12,11 @@ class Message {
  
 class NetworkService {
   bool meth = false;
-  late NearbyService nearbyService;
-
+  Random rand = Random();
+  NearbyService nearbyService = NearbyService();
+  List<Device> connectedDevices = [];
   void init() async {
-     nearbyService = NearbyService();
+     //nearbyService = NearbyService();
     
     await nearbyService.init(
       serviceType: 'mpconn',
@@ -22,18 +24,38 @@ class NetworkService {
       strategy: Strategy.P2P_CLUSTER,
       callback: (isrunning) async {
         if(isrunning){
-  
+          if(rand.nextBool()){
+            await nearbyService.startAdvertisingPeer();
+            await nearbyService.startBrowsingForPeers();
             await nearbyService.stopAdvertisingPeer();
             await nearbyService.stopBrowsingForPeers();
             await Future.delayed(const Duration(microseconds: 200));
-            await nearbyService.startAdvertisingPeer();
+            }
+          }
+          else {
             await nearbyService.startBrowsingForPeers();
+            await nearbyService.stopBrowsingForPeers();
+            await Future.delayed(const Duration(microseconds: 200));
           }
         }
       );
+      StreamSubscription subscription = nearbyService.stateChangedSubscription(
+        callback: (deviceList) {
+          connectedDevices.clear();
+          for (Device element in deviceList) { 
+            if(element.state==SessionState.notConnected){
+            nearbyService.invitePeer(deviceID: element.deviceId, deviceName: element.deviceName);}
+            else if(element.state==SessionState.connected){
+              connectedDevices.add(element);
+            }
+          }
+        }
+   ); 
     StreamSubscription receivedDataSubscription = nearbyService.dataReceivedSubscription(callback: (data){
-
+      //TODO check this
+      LocalDB().saveData("messages", data.name, data);
    });
+
   }
   
 }
