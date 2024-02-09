58c41a11f15f554152a92b06f03e91987dce0e92
dokiii <dogukan.bayhan@sabanciuniv.edu>
2024-02-09 23:28:35 +0300

    sdfsdf

--- lib/pages/messages/messages_main_page.dart
+++ lib/pages/messages/messages_main_page.dart
@@ -81,6 +81,63 @@ class _MessagesMainPageState extends State<MessagesMainPage> {
                 ),
               ),
             ),
+            Container(
+              child: MessagesList(isMessageRead: true,name: "Zeynep", time: "Today", messageText: "sdfsdf",),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class MessagesList extends StatefulWidget{
+  String name;
+  String messageText;
+  String time;
+  bool isMessageRead;
+
+  MessagesList({required this.name,required this.isMessageRead,required this.time,required this.messageText});
+
+  @override
+  _MessagesListState createState() => _MessagesListState();
+}
+
+class _MessagesListState extends State<MessagesList> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    maxRadius: 30,
+                  ),
+                  SizedBox(width: 16,),
+                  Expanded(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(widget.name, style: TextStyle(fontSize: 16),),
+                          SizedBox(height: 6,),
+                          Text(widget.messageText,style: TextStyle(fontSize: 13,color: Colors.grey.shade600, fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(widget.time,style: TextStyle(fontSize: 12,fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
           ],
         ),
       ),
