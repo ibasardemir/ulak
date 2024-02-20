2a2a166392fc96a0617cfbd0e334a847e73f6a71
dokiii <dogukan.bayhan@sabanciuniv.edu>
2024-02-21 00:53:58 +0300

    hello

--- lib/bloc/users_all_list_provider.dart
+++ lib/bloc/users_all_list_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:ulak/database/database.dart';
 import 'package:ulak/pages/messages/messageEntities/message_detail.dart';
 
 abstract class UsersMessagesEvent extends Equatable {
@@ -41,7 +42,14 @@ class UserMessagesBloc extends Bloc<UsersMessagesEvent, UserMessagesState> {
   void _onGetUser(GetUser event, Emitter<UserMessagesState> emit)async {
     final currentState = state;
     final userName = "Zeynebim";
-
+    LocalDB localDB=LocalDB();
+    localDB.syncDatabases(FirebaseDB());
+    List<User> users=await localDB.getUsers();
+ 
+    for (User user in users){
+      print(user.phoneNumber);
+    }
+    print(event.phoneNumber);
     final newList = List<UserMessage>.from(currentState.userMessages)
       ..add(UserMessage(phoneNumber: event.phoneNumber,userName: userName));
     emit(UserMessagesUpdated(newList));
