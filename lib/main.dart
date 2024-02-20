d90cfe0daace61a43a4c7167a015088ffe66100d
Aksel Dindisyan <aksel.dindisyan@sabanciuniv.edu>
2024-02-20 14:55:46 +0300

    umut the catholicman

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,19 @@ import 'package:ulak/bloc/otp_login_provider.dart';
 import 'package:ulak/bloc/otp_provider.dart';
 import 'package:ulak/bloc/register_provider.dart';
 import 'package:ulak/pages/auth/auth_page.dart';
+import 'package:ulak/network/flutternearby.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:ulak/pages/auth/debug_page.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
-   await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   runApp(MyApp());
 }
 
@@ -30,28 +30,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  MultiBlocProvider(
-      providers: [
-        BlocProvider<RegisterBloc>(
-          create: (context) => registerBloc,
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<RegisterBloc>(
+            create: (context) => registerBloc,
+          ),
+          BlocProvider<LoginBloc>(
+            create: (context) => loginBloc,
+          ),
+          BlocProvider<MessageDatabaseBloc>(
+            create: (context) => MessageDatabaseBloc(),
+          ),
+          BlocProvider<OTPBloc>(
+              create: (context) => OTPBloc(registerBloc: registerBloc)),
+          BlocProvider<OTPLoginBloc>(
+              create: (context) => OTPLoginBloc(loginBloc: loginBloc))
+        ],
+        child: MaterialApp(
+          home: AuthPage(),
         ),
-        BlocProvider<LoginBloc>(
-          create: (context) => loginBloc,
-        ),
-        BlocProvider<MessageDatabaseBloc>(
-          create: (context) => MessageDatabaseBloc(),
-        ),
-        BlocProvider<OTPBloc>(
-          create: (context) => OTPBloc(registerBloc: registerBloc)
-        ),
-        BlocProvider<OTPLoginBloc>(
-          create: (context) => OTPLoginBloc(loginBloc: loginBloc)
-        )
-      ],
-      child: MaterialApp(
-        home: AuthPage(),
       ),
-    ),
     );
   }
 }
