4b6cf61115fd8403a3ba564a00dff7bb3fac1c7e
dokiii <dogukan.bayhan@sabanciuniv.edu>
2024-02-20 15:32:36 +0300

    sdfsdf

--- lib/network/networkservice.dart
+++ lib/network/networkservice.dart
@@ -1,15 +1,19 @@
 import "dart:async";
 import "dart:math";
 import "package:flutter_nearby_connections/flutter_nearby_connections.dart";
-import "package:shared_preferences/shared_preferences.dart";
 import "package:ulak/database/auth.dart";
-import "package:ulak/database/database.dart" as database;
+import "package:ulak/database/database.dart";
 
 /*
 Network Service: 
   Function sendMessage input:Message object, sends message to the network.
 */
 
+class Message {
+  int time;String sender;String receiver; String content;
+  Message(this.time, this.sender, this.receiver,this.content);
+}
+ 
 class NetworkService {
   bool meth = false;
   Random rand = Random();
@@ -18,75 +22,91 @@ class NetworkService {
 
   late StreamSubscription subscription;
   late StreamSubscription receivedDataSubscription;
-  StreamController<List<Device>> controller = StreamController<List<Device>>();
+  StreamController<List<Device>> controller= StreamController<List<Device>>();
   late Stream stream;
   void init({bool meths = false}) async {
     meth = meths;
+    print("object");
     await nearbyService.init(
-        serviceType: 'mpconn',
-        deviceName: phoneNumber, //await Authentication().returnPhoneNum(),
-        strategy: Strategy.P2P_CLUSTER,
-        callback: (isrunning) async {
-          if (isrunning) {
-            print("new call");
-            if (meth) {
+      serviceType: 'mpconn',
+      deviceName: "Fuhrer",//await Authentication().returnPhoneNum(),
+      strategy: Strategy.P2P_CLUSTER,
+      callback: (isrunning) async {
+        if(isrunning){
+          print("new call");
+          if(meth){
+            await nearbyService.startBrowsingForPeers();
+            await Future.delayed(const Duration(microseconds: 200));
+            for(int i= 0;i<10;i++){
               await nearbyService.stopBrowsingForPeers();
               await Future.delayed(const Duration(microseconds: 200));
               await nearbyService.startBrowsingForPeers();
-            } else {
+            /*await nearbyService.startAdvertisingPeer();
+            await nearbyService.startBrowsingForPeers();
+            await nearbyService.stopAdvertisingPeer();
+            await nearbyService.stopBrowsingForPeers();
+            await Future.delayed(const Duration(microseconds: 200));*/
+            }
+            await nearbyService.stopBrowsingForPeers();
+            }
+          }
+          else {
+            await nearbyService.startAdvertisingPeer();
+            await nearbyService.startBrowsingForPeers();
+            await Future.delayed(const Duration(microseconds: 200));
+            for(int i=0;i<10;i++){
               await nearbyService.stopAdvertisingPeer();
               await nearbyService.stopBrowsingForPeers();
               await Future.delayed(const Duration(microseconds: 200));
               await nearbyService.startAdvertisingPeer();
               await nearbyService.startBrowsingForPeers();
+            /*await nearbyService.startBrowsingForPeers();
+            await nearbyService.stopBrowsingForPeers();
+            await Future.delayed(const Duration(microseconds: 200));*/}
+            await nearbyService.stopAdvertisingPeer();
+            await nearbyService.stopBrowsingForPeers();
+          }
+        }
+      );
+      void sendMessage(Message message, String deviceId)  {
+        try {
+          nearbyService.sendMessage(deviceId, message.content);
+        }
+        catch(e) {
+          //TODO do this
+          print("Eyvah");
+        } 
+     //TODO do this
+
+  }
+      stream = controller.stream;
+      subscription = nearbyService.stateChangedSubscription(
+        callback: (deviceList) {
+          connectedDevices.clear();
+          for (Device element in deviceList) { 
+            if(element.state==SessionState.notConnected){
+            nearbyService.invitePeer(deviceID: element.deviceId, deviceName: element.deviceName);}
+            else if(element.state==SessionState.connected){
+              connectedDevices.add(element);
             }
           }
-        });
-    void sendMessage(database.Message message, String deviceId) async {
-      try {
-        nearbyService.sendMessage(deviceId, message.message ?? "");
-      } catch (e) {
-        ;
-      }
-      //TODO do this
-    }
-
-    stream = controller.stream;
-    subscription =
-        nearbyService.stateChangedSubscription(callback: (deviceList) {
-      connectedDevices.clear();
-      for (Device element in deviceList) {
-        if (element.state == SessionState.notConnected) {
-          nearbyService.invitePeer(
-              deviceID: element.deviceId, deviceName: element.deviceName);
-        } else if (element.state == SessionState.connected) {
-          connectedDevices.add(element);
+          controller.add(connectedDevices);
         }
-      }
-      controller.add(connectedDevices);
-    });
-
-    receivedDataSubscription =
-        nearbyService.dataReceivedSubscription(callback: (data) async {
+   );
+   
+    receivedDataSubscription = nearbyService.dataReceivedSubscription(callback: (data){
       //TODO check this
-      for (var dev in connectedDevices) {
-        if (dev.deviceId != data.senderDeviceId)
-          sendMessage(data.content, dev.deviceId);
+      for (var dev in connectedDevices){
+        if(dev.deviceId!=data.senderDeviceId)sendMessage(data.content,dev.deviceId);
       }
-
+      
       //TODO: Başar Buraya bak
       // *Database.dart message class yolla*
-
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? phoneNumber = prefs.getString("phoneNumber");
-
-      database.Message message = database.Message(
-          sender: data.deviceId, reciever: phoneNumber, message: data.message);
-      database.LocalDB().saveMessage(message);
-    });
+      LocalDB().saveMessage(data);
+   });
   }
-
-  StreamSubscription returnSubscription() {
+  
+  StreamSubscription returnSubscription (){
     return controller.stream.listen((event) {});
-  }
+   } 
 }
